cb2ff16fb6fdf8833db9f00621bd0be34e8533ac
i2bskn <i2bskn@gmail.com>
2018-06-14 16:54:03 +0900

    update

--- test/sql_migrate_test.rb
+++ test/sql_migrate_test.rb
@@ -4,8 +4,4 @@ class SqlMigrateTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::SqlMigrate::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
